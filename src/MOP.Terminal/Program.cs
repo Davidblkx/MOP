09619737cbc7cd9070e568716a3402e88a195ecb
David Pires <developer@davidpires.pt>
2020-07-12 11:52:28 +0100

    Added base coms projects

---
+++ src/MOP.Terminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MOP.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
