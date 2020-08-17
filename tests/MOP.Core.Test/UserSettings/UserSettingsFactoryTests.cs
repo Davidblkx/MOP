20163edd45b1a9936e0fccd4da566c11c2817de4
David Pires <developer@davidpires.pt>
2020-08-17 02:37:10 +0100

    Added tests to core

---
+++ tests/MOP.Core.Test/UserSettings/UserSettingsFactoryTests.cs
@@ -0,0 +1,39 @@
+﻿using MOP.Core.UserSettings;
+using Xunit;
+
+namespace MOP.Core.Test.UserSettings
+{
+    public class UserSettingsFactoryTests
+    {
+        [Fact]
+        public void TestSetDefaultValue()
+        {
+            var targetValue = new TestClass { Name = "batatas", Age = 30 };
+            var target = UserSettingsFactory
+                .Create<TestClass>()
+                .SetDefaultValue(targetValue);
+
+            Assert.NotNull(target);
+            Assert.Equal(targetValue, target.DefaultValue);
+        }
+
+        [Fact]
+        public void TestUseDefaultValue()
+        {
+            var target = UserSettingsFactory
+                .Create<TestClass>();
+
+            Assert.NotNull(target);
+            var def = target.DefaultValue;
+            Assert.NotNull(def);
+            Assert.Equal("Unknown", def.Name);
+            Assert.Equal(10, def.Age);
+        }
+    }
+
+    internal class TestClass
+    {
+        public string Name { get; set; } = "Unknown";
+        public int Age { get; set; } = 10;
+    }
+}
